cdfc3f6fc1fcde168b84ccfc69f34e01a6aee6dd
krist-ig-project <153905051+krist-ig-project@users.noreply.github.com>
2024-01-10 22:34:31 +0100

    Update index.js

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,12 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAzfkwM8Un7EuJB6MUzpGKRdR1KictzPDQ",
-  authDomain: "backup-bakcup.firebaseapp.com",
-  projectId: "backup-bakcup",
-  storageBucket: "backup-bakcup.appspot.com",
-  messagingSenderId: "159336108659",
-  appId: "1:159336108659:web:5b55de1d02d33912a65006"
+  apiKey: "AIzaSyAxxx9Ubspay9DsNHKu300yrr7M69AL7WU",
+  authDomain: "first-service-24499.firebaseapp.com",
+  projectId: "first-service-24499",
+  storageBucket: "first-service-24499.appspot.com",
+  messagingSenderId: "749988043724",
+  appId: "1:749988043724:web:0fd2c24294aaf77e261b69",
+  measurementId: "G-F9KN4WZR08"
 };
 
 
