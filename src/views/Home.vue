78121ffc06f96bf1b2c16ab10b2c6c4bacf46805
krist-ig-project <153905051+krist-ig-project@users.noreply.github.com>
2024-01-10 22:39:34 +0100

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
     <form @submit.prevent="fetchSubcollectionData">
       <label for="savedId">Enter Value:</label>
       <input v-model="savedId" type="text" id="savedId" />
-      <button type="submit">Submit</button>
+      <button type="submit" class="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Submit</button>
     </form>
 
     <div class="p-1 dark:bg-gray-800 min-h-screen border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
