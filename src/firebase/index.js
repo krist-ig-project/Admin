50c86134da7b0ee8c741efcd2e301c4f057880db
krist-ig-project <153905051+krist-ig-project@users.noreply.github.com>
2024-01-19 10:21:42 +0100

    Update index.js

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,14 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCPgmW_mg4-WrDxon6HSIYZ2DMgpcvVL_0",
-  authDomain: "backup-forums.firebaseapp.com",
-  projectId: "backup-forums",
-  storageBucket: "backup-forums.appspot.com",
-  messagingSenderId: "107728706395",
-  appId: "1:107728706395:web:035f203c9492e2b23b187e",
-  measurementId: "G-P7BE174QQ0"
+  apiKey: "AIzaSyAxxx9Ubspay9DsNHKu300yrr7M69AL7WU",
+  authDomain: "first-service-24499.firebaseapp.com",
+  projectId: "first-service-24499",
+  storageBucket: "first-service-24499.appspot.com",
+  messagingSenderId: "749988043724",
+  appId: "1:749988043724:web:0fd2c24294aaf77e261b69",
+  measurementId: "G-F9KN4WZR08"
 };
+
 
 
 
