968d932c4a08fac1bb24aec386e6fe560c205c51
krist-ig-project <153905051+krist-ig-project@users.noreply.github.com>
2024-08-20 09:00:01 +0100

    Update Form-submit.js

--- src/Form-submit.js
+++ src/Form-submit.js
@@ -1,6 +1,19 @@
 // Form-submit.js
 import { db } from '/src/firebase/index.js';
+import { collection, addDoc } from 'firebase/firestore';
 
 export function HandleSubmitedForm(formData) {
-  I want to send the data to Firebase
+  // Reference to the collection where you want to store the data
+  const formCollectionRef = collection(db, 'formSubmissions');
+  
+  // Return a promise that resolves when the data is successfully added
+  return addDoc(formCollectionRef, formData)
+    .then(() => {
+      // Successfully added document
+      return Promise.resolve();
+    })
+    .catch((error) => {
+      // Error occurred while adding document
+      return Promise.reject(error);
+    });
 }
